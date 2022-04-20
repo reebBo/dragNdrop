5ac8abf2d92e55cb78e5fba604dbf32b8115a95c
rebecaLH <76942384+rebecaLH@users.noreply.github.com>
2022-04-20 20:18:43 +0200

    delete by id

--- src/app/services/layout.service.ts
+++ src/app/services/layout.service.ts
@@ -7,14 +7,14 @@ export interface IComponent {
   componentRef: string;
 }
 
-const OptionsGridsterConfig:GridsterConfig={
+const OptionsGridsterConfig: GridsterConfig = {
   draggable: {
     enabled: true
   },
   pushItems: true,
   resizable: {
     enabled: true
-  } 
+  }
 }
 
 @Injectable({
@@ -23,7 +23,7 @@ const OptionsGridsterConfig:GridsterConfig={
 
 
 export class LayoutService {
- 
+
 
   // public options: GridsterConfig = {
   //   draggable: {
@@ -40,49 +40,49 @@ export class LayoutService {
   //   // maxItemCols:5,
   //   // maxItemArea:35
   // }; 
-  
-  getConfig():GridsterConfig{
+
+  getConfig(): GridsterConfig {
     return OptionsGridsterConfig;
   }
 
   public layout: GridsterItem[] = [
-    {cols:1, rows:2, x: 0, y: 0},
-    {cols:1, rows:1, x: 0, y: 0},
-    {cols:1, rows:1, x: 0, y: 0},
-    {cols:1, rows:1, x: 0, y: 0},
-    {cols:1, rows:1, x: 0, y: 0},
-    {cols:1, rows:1, x: 0, y: 0},
-    {cols:1, rows:2, x: 0, y: 0},
-    {cols:1, rows:1, x: 0, y: 0},
-    {cols:1, rows:1, x: 0, y: 0},
-    {cols:1, rows:1, x: 0, y: 0},
-    {cols:1, rows:2, x: 0, y: 0},
-    {cols:1, rows:1, x: 0, y: 0},
-    {cols:1, rows:1, x: 0, y: 0}
+    { cols: 1, rows: 2, x: 0, y: 0, id: 1 },
+    { cols: 1, rows: 1, x: 0, y: 0, id: 2 },
+    { cols: 1, rows: 1, x: 0, y: 0, id: 3 },
+    { cols: 1, rows: 1, x: 0, y: 0, id: 4 },
+    { cols: 1, rows: 1, x: 0, y: 0, id: 5 },
+    { cols: 1, rows: 1, x: 0, y: 0, id: 6 },
+    { cols: 1, rows: 2, x: 0, y: 0, id: 7 },
+    { cols: 1, rows: 1, x: 0, y: 0, id: 8 },
+    { cols: 1, rows: 1, x: 0, y: 0, id: 9 },
+    { cols: 1, rows: 1, x: 0, y: 0, id: 10 },
+    { cols: 1, rows: 2, x: 0, y: 0, id: 11 },
+    { cols: 1, rows: 1, x: 0, y: 0, id: 12 },
+    { cols: 1, rows: 1, x: 0, y: 0, id: 13}
 
-  ]; 
+  ];
 
-  constructor() { } 
-  
+  constructor() { }
+
   addItem(): void {
     this.layout.push(
-    //   {
-    //   cols: 5,
-    //   id: UUID.UUID(),
-    //   rows: 5,
-    //   x: 0,
-    //   y: 0
-    // }
-    {cols:1, rows:1, x: 0, y: 0},
+      //   {
+      //   cols: 5,
+      //   id: UUID.UUID(),
+      //   rows: 5,
+      //   x: 0,
+      //   y: 0
+      // }
+      { cols: 1, rows: 1, x: 0, y: 0, id: UUID.UUID() },
 
     );
-  } 
-  
-  
-  deleteItem(id: any): void {
-    const item:any = this.layout.find(d => d.id === id);
-    this.layout.splice(this.layout.indexOf(item), 1);
   }
+
+
+  deleteItem(item: any) {
+    const itemId: any = this.layout.find(d => d.id === item.id);
+    this.layout.splice(this.layout.indexOf(itemId), 1);
+  } 
 }
 
 
